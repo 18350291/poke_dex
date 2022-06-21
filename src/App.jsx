aaa346d5af1dae51b4a3cbc2d6583413c95a675c
Alex-2699 <alexantonio262699@gmail.com>
2022-06-20 19:24:38 -0500

    Desarrollo base del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h3>Poke Api</h3>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+    </div>
+  );
+}
+
+export default App;
